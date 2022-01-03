c060f2c1ebc0b7e76f4f5340c3398aee26e63311
Noel Wilson <jwnwilson@hotmail.co.uk>
2022-01-03 02:28:27 +0000

    Updating timeout for worker

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -83,6 +83,7 @@ module "pdf_worker" {
   # vpc_subnet_ids          = module.vpc.private_subnets
   # vpc_security_group_ids  = [module.vpc.default_security_group_id]
   attach_network_policy   = true
+  timeout                 = 900
 
   environment_variables = {
     ENVIRONMENT = var.environment
